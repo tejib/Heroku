847494943dd72c4d2d3bf7813f44d5a8b1eeda8e
Tej <me.tonyb@gmail.com>
2020-04-04 04:31:59 -0400

    Added PostgreSQL as DB for Heroku

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #from db import db
+import os
 
 from flask import Flask
 from flask_restful import Resource, Api
@@ -10,7 +11,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '####'
 api = Api(app)
