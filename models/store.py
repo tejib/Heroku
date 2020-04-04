1bac2b12b912515c2254d9bee205d76f9fee361c
Tej <me.tonyb@gmail.com>
2020-04-04 03:59:02 -0400

    store model file changed

--- models/store.py
+++ models/store.py
@@ -13,7 +13,8 @@ class StoreModel(db.Model):
         self.name = name
 
     def json(self):
-        return {'name': self.name,'items': [item.json() for item in self.items.all()]}
+        #return {'name': self.name,'items': [item.json() for item in self.items.all()]}
+        return {'store_id':id,'name': self.name,'items': [item.json() for item in self.items.all()]}
 
     @classmethod
     def find_by_name(cls, name):
